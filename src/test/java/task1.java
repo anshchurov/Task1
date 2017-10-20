cbd6fbb5d8a6484abf8c7582479560e24cd16d7a
anshchurov <anshchurov@gmail.com>
2017-10-20 14:39:33 +0300

    Initial commit

---
+++ src/test/java/task1.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class task1 {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        baseUrl = "http://www.sberbank.ru/ru/person/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+
+}
